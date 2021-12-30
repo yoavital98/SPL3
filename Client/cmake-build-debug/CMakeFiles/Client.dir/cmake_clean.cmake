a49d7d636c1780ab1cc28e0c83c03b641a85a099
oranbn <oranbenn@post.bgu.ac.il>
2021-12-30 19:08:39 +0200

    new update

---
+++ Client/cmake-build-debug/CMakeFiles/Client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client.dir/main.cpp.obj"
+  "Client.exe"
+  "Client.exe.manifest"
+  "Client.pdb"
+  "libClient.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
